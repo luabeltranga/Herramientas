c75a6357a1c1e9d07131190f94168a312686f4d7
luabeltranga <luabeltranga@unal.edu.co>
2017-03-03 09:09:57 -0500

    programas de prueba

--- ejercicios/error_seno.cpp
+++ ejercicios/error_seno.cpp
@@ -1,48 +1,86 @@
-#include <iostream>
-#include <cmath>
-#include <cstdio>
+// From : https://github.com/undees/fftw-example
 
-double serie (double x );
+/* Start reading here */
 
-int main(void)
+#include <fftw3.h>
 
-{ 
+#define NUM_POINTS 128
+
+/* Never mind this bit */
+
+#include <stdio.h>
+
+#include <math.h>
+
+#define REAL 0
+
+#define IMAG 1
+
+void acquire_from_somewhere(fftw_complex* signal) {
+  
+  /* Generate two sine waves of different frequencies and
    
+* amplitudes.
 
-  double z=1; 
+*/
   
+  int i;
+ 
+ for (i = 0; i < NUM_POINTS; ++i) {
   
-
-   for (int j=1;j<=1000;j++)
-    {
-      z=j*0.1;
-      std::printf("%f %20.16e \n", z ,fabs((serie(z)-std::sin(z))/std::sin(z)));
-    }
-
-  /*  double suma =0, eps = pow(10,-7), x=2M_PI, it=x ;
-  int j=1;
-  do 
-    {suma+=it;
-      it*=-x*x/(2*j+1)/(2*j);
-      std::printf("%i %20.16e \n", j,fabs(suma-std::sin(x))/std::sin(x) );
-      j++;
-    }
-  while (fabs(it/suma)>eps );*/
+  double theta = (double)i / (double)NUM_POINTS * M_PI;
+ 
+ signal[i][REAL] = 1.0 * cos(10.0 * theta) +
   
-return 0;
+  0.5 * cos(25.0 * theta);
+ 
+ signal[i][IMAG] = 1.0 * sin(10.0 * theta) +
+  
+  0.5 * sin(25.0 * theta);
+ 
+ }
+ 
 }
-double serie (double x)
-{
-  double suma =0, it=x, eps = pow(10,-7),j=1;
+
+void do_something_with(fftw_complex* result) {
   
+  int i;
+ 
+ for (i = 0; i < NUM_POINTS; ++i) {
+  
+double mag = sqrt(result[i][REAL] * result[i][REAL] +
+		  
+		  result[i][IMAG] * result[i][IMAG]);
+ 
+printf("%g\n", mag);
+ 
+ }
+ 
+}
 
- do 
-   {suma+=it;
-     it*=-x*x/(2*j+1)/(2*j);
-      j++;
-  }
- while (fabs(it/suma)>eps );
+/* Resume reading here */
 
-  return suma;
+int main() {
 
+  fftw_complex signal[NUM_POINTS];
+ 
+ fftw_complex result[NUM_POINTS];
+ fftw_plan plan = fftw_plan_dft_1d(NUM_POINTS,
+
+				   signal,
+
+				   result,
+
+				   FFTW_FORWARD,
+
+				   FFTW_ESTIMATE);
+ 
+ acquire_from_somewhere(signal);
+ 
+ fftw_execute(plan);
+ 
+ do_something_with(result);
+
+ fftw_destroy_plan(plan);
+ return 0;
 }
