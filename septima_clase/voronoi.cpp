08a330088bb9b993fef765f4f9c0a050c224569c
luabeltranga <luabeltranga@unal.edu.co>
2017-03-04 22:41:26 -0500

    yuju

--- septima_clase/voronoi.cpp
+++ septima_clase/voronoi.cpp
@@ -35,51 +35,51 @@ const int particles=20;
 double rnd() {return double(rand())/RAND_MAX;}
 
 int main() {
+  
+  int i;
+  
+  double x,y,z;
+  
+  // Create a container with the geometry given above, and make it
 
-int i;
-
-double x,y,z;
-
-// Create a container with the geometry given above, and make it
-
-// non-periodic in each of the three coordinates. Allocate space for
-
-// eight particles within each computational block
-
-container con(x_min,x_max,y_min,y_max,z_min,z_max,n_x,n_y,n_z,
-
-false,false,false,8);
-
-// Randomly add particles into the container
-
-for(i=0;i<particles;i++) {
-
-x=x_min+rnd()*(x_max-x_min);
-
-y=y_min+rnd()*(y_max-y_min);
-
-z=z_min+rnd()*(z_max-z_min);
-
-con.put(i,x,y,z);
-
-}
-
-// Sum up the volumes, and check that this matches the container volume
-
-double vvol=con.sum_cell_volumes();
-
-printf("Container volume : %g\n"
-
-"Voronoi volume : %g\n"
-
-"Difference : %g\n",cvol,vvol,vvol-cvol);
-
-// Output the particle positions in gnuplot format
-
-con.draw_particles("random_points_p.gnu");
-
+  // non-periodic in each of the three coordinates. Allocate space for
+  
+  // eight particles within each computational block
+  
+  container con(x_min,x_max,y_min,y_max,z_min,z_max,n_x,n_y,n_z,
+		
+		false,false,false,8);
+  
+  // Randomly add particles into the container
+  
+  for(i=0;i<particles;i++) {
+    
+    x=x_min+rnd()*(x_max-x_min);
+    
+    y=y_min+rnd()*(y_max-y_min);
+    
+    z=z_min+rnd()*(z_max-z_min);
+    
+    con.put(i,x,y,z);
+    
+  }
+  
+  // Sum up the volumes, and check that this matches the container volume
+  
+  double vvol=con.sum_cell_volumes();
+  
+  printf("Container volume : %g\n"
+	 
+	 "Voronoi volume : %g\n"
+	 
+	 "Difference : %g\n",cvol,vvol,vvol-cvol);
+  
+  // Output the particle positions in gnuplot format
+  
+  con.draw_particles("random_points_p.gnu");
+  
 // Output the Voronoi cells in gnuplot format
-
+  
 con.draw_cells_gnuplot("random_points_v.gnu");
-
+ 
 }
