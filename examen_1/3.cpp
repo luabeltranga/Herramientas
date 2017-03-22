9935cb6f13bf02d4f21a2af43b4f9e1e009b53cb
luabeltranga <luabeltranga@unal.edu.co>
2017-03-22 11:55:21 -0500

    yuju

---
+++ examen_1/3.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Eigenvalues>
+
+void set_H0(Eigen::MatrixXd & M);
+
+void set_X(Eigen::MatrixXd & M);
+
+double eigen_energy(Eigen::MatrixXd & H, Eigen::MatrixXd & X, const double lambda, const int index);
+
+int main(int argc, char **argv)
+  
+{
+  
+  std::cout.precision(16);
+  
+  std::cout.setf(std::ios::scientific);
+  
+  double lambda  = 0.2;
+ for (int N = 2; N <= 1024; N *=2) {
+
+   
+   Eigen::MatrixXd X(N, N), H0(N, N);
+   
+   set_H0(H0);
+   
+   set_X(X);
+   
+   std::cout << 1.0/N << " " << eigen_energy(H0, X, lambda, 0) << std::endl;
+   
+ }
+
+ return 0;
+ 
+}
+
+void set_H0(Eigen::MatrixXd & M)
+  
+{
+  
+  M.setZero();
+  
+  // Escriba aca el codigo que crea la matriz H0
+  
+  for (int n = 0; n < M.cols(); ++n){
+    
+    for (int m = 0; m < M.cols(); ++m){
+      
+      if (n == m){
+	M(n, m) = n +0.5 ;
+      }
+    }
+    
+  }
+    
+// Puede guiarse por la funcion set_X
+    
+    }
+
+void set_X(Eigen::MatrixXd & M)
+  
+{
+  
+  
+  M.setZero();
+  
+  
+  
+  
+  for (int n = 0; n < M.cols(); ++n){
+    
+    for (int m = 0; m < M.cols(); ++m){
+      
+      if (n == m+1) {
+	M(n, m) += std::sqrt((m+1.0)/2.0);
+      }
+      if (n == m-1) {
+	M(n, m) += std::sqrt((m)/2.0);
+      }
+    }
+    
+  }
+  
+  
+  
+}
+
+double eigen_energy(Eigen::MatrixXd & H, Eigen::MatrixXd & X, const double lambda, const int index)
+  
+{
+
+  // Implemente aca el calculo de los valores propios, usando la libreria eigen
+  
+  // - Calculo de Hlambda :
+
+  Eigen::MatrixXd M(H.cols(), H.cols());
+
+  
+  M=H+lambda*X*X*X*X;
+  
+ 
+  
+  // - Calculo de los valores propios (y vectores propios) :
+  
+
+    
+    // - Extraer los valores propios al vector Eigen::VectorXd evals, solamente
+  Eigen::VectorXd evals = M.eigenvalues().real();
+    
+
+    // la parte real (cuando pida los valores propios escriba .eigenvalues.real()) :
+    
+    // ordenar los valores propios :
+    
+    std::sort(evals.data(), evals.data() + evals.size());
+  
+  // retornar el valor propio (para el estado base index == 0) :
+  
+  return evals(index);
+  
+}
--- linear_algebra_exercise/LU_QR_solve/Eigen.cpp
+++ linear_algebra_exercise/LU_QR_solve/Eigen.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <Eigen/Dense>
-
+#include <cmath>
+#include <Eigen/Eigenvalues>
 void solver(void);
 
 int main(void){
@@ -12,18 +13,35 @@ int main(void){
 
 void solver(void){
 
+  Eigen::MatrixXd a(4,4);
   Eigen::Matrix4d A;
-  A << 0.18, 0.60, 0.57, 0.96,
+   A << 0.18, 0.60, 0.57, 0.96,
     0.41, 0.24, 0.99, 0.58,
     0.14, 0.30, 0.97, 0.66,
     0.51, 0.13, 0.19, 0.85;
-  Eigen::Vector4d b;
+
+   Eigen::Vector4d b;
   b<< 1.0, 2.0, 3.0, 4.0;
 
   //Eigen::Vector4d x=A.lu().solve(b);
   //Eigen::Vector4d x=A.Qr():solve(b);
   //Eigen::Vector4d x=A.solve(b);
-  std::cout<<x<<std::endl;
+  
+  for (int n = 0; n < 4; ++n){
+    
+    for (int m = 0; m < 4; ++m){
+      
+      if (n == m+1) {
+	a(n,m) += A(n,m)*A(n,m);
+      }
+      if (n == m-1){
+	a(n,m) += 1;
+      }
+    }
+    
+  }
+  Eigen::VectorXd y=a.eigenvalues().real();
+  std::cout<<y*A<<std::endl;
         
 
 }
