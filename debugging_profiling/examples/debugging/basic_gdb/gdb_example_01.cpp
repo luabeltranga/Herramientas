5de373954ffb044e9a1fb0e4be268b6de6092861
Slackware Live User <live@darkstar.example.net>
2017-03-26 16:49:20 -0700

    yuju

--- debugging_profiling/examples/debugging/basic_gdb/gdb_example_01.cpp
+++ debugging_profiling/examples/debugging/basic_gdb/gdb_example_01.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <cstdlib>
 
-int main(void) 
+int main(int argc, char **argv) 
 {
   int a;
+  int b=5;
   a = 0;
   std::cout << a << std::endl;
   a = 5;
 
-  return 0;
+    return EXIT_SUCCESS;
 }
