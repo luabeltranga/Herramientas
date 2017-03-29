ee7b69921e6c4e0fe547be3c43b74bafee875a4f
luabeltranga <luabeltranga@unal.edu.co>
2017-03-29 07:16:13 -0500

    yuju

--- debugging_profiling/array_error.cpp
+++ debugging_profiling/array_error.cpp
@@ -1,15 +1,13 @@
 #include<iostream>
 
 int main(void){
+  double array[10]{1,2,3,4,5,6,7,8,9,10};
   double array2[2000]{0};
-  double array[11]{0,1,2,3,4,5,6,7,8,9,10};
+
  
   for(auto &x : array2) x=10;
+  std::cout<<array[0]<<std::endl;
+  std::cout<<array[2000]<<std::endl;
 
-  std::cout<<array2[11]<<std::endl;
-
-  for (int jj : array){
-    std::cout<<array[jj]<<std::endl;  }
-    std::cout<<array[200]<<std::endl;  
   return 0;
 }
---
+++ debugging_profiling/debugging.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void){
+  double array[10]{1,2,3,4,5,6,7,8,9,10};
+  double array2[2000] {0};   //si se cambia el orden de la declaracion ocurre un error
+  
+  for(auto & val :array2) val=-10;
+  std::cout<<array[0]<<std::endl;
+  std::cout<<array[2000]<<std::endl;
+  
+  return 0;
+}
