e735cdf186c545cd6c47156d395b8369888065b5
luabeltranga <luabeltranga@unal.edu.co>
2017-04-21 11:38:51 -0500

    yaju

--- parallelism/openmp_array.cpp
+++ parallelism/openmp_array.cpp
@@ -4,29 +4,26 @@
 #include <iostream>
 int main(void) {
   
-  const long N = 10;
-  long sum  ;
+  const long N = 100000;
+  long sum=0 ;
   int i, *a;
   a=new int[N];
-#pragma omp parallel num_threads (4)  
+#pragma omp parallel num_threads(4)
   {
 #pragma omp parallel for
-  for(i = 0; i < N; i++)
-    
-    a[i] = 1 ;
+    for(i = 0; i < N; i++)
+      
+      a[i] = 1 ;
   }
   
-#pragma omp parallel 
-  
-  {
-#pragma omp  for   reduction(+:sum) 
-    for(int  i = 0; i < N; i++)
+#pragma omp parallel  for  reduction(+:sum) num_threads(5) 
+   for(int  i = 0; i < N; i++)
       {
 	sum +=  a[i];
 	
       }
     
-  }
+  
   std::cout << sum << std::endl;
   delete [] a;
   return 0;
